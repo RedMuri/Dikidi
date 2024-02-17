edbb362e09a5d49f80be05bd7b23365f29fc02ed
RedMuri <celovekda@gmail.com>
2024-02-17 13:00:20 +0300

    Add fonts

--- app/src/main/java/com/example/dikidi/ui/theme/Type.kt
+++ app/src/main/java/com/example/dikidi/ui/theme/Type.kt
@@ -2,9 +2,17 @@ package com.example.dikidi.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.dikidi.R
+
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_400, FontWeight.Normal),
+    Font(R.font.open_sans_500, FontWeight.Medium),
+    Font(R.font.open_sans_600, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
