3ca04f560359b09b066f7e762dd414ebae1c34f8
RedMuri <celovekda@gmail.com>
2024-03-04 01:08:24 +0300

    Inject store factory

--- app/src/main/java/com/example/dikidi/MainActivity.kt
+++ app/src/main/java/com/example/dikidi/MainActivity.kt
@@ -5,20 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.example.dikidi.ui.theme.DikidiTheme
-import com.example.dikidi.ui.viewmodel.ViewModelFactory
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
 
-    private val component by lazy {
-        (application as DikidiApp).component
-    }
-
     @Inject
-    lateinit var viewModelFactory: ViewModelFactory
+    lateinit var rootComponentFactory: DefaultRootComponent.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        component.inject(this)
+        (applicationContext as DikidiApp).applicationComponent.inject(this)
         super.onCreate(savedInstanceState)
 
         enableRenderUnderSystemBar()
--- app/src/main/java/com/example/dikidi/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dikidi/di/ApplicationComponent.kt
@@ -1,12 +1,12 @@
 package com.example.dikidi.di
 
 import android.app.Application
-import com.example.dikidi.ui.main.MainActivity
+import com.example.dikidi.MainActivity
 import dagger.BindsInstance
 import dagger.Component
 
 @ApplicationScope
-@Component(modules = [DataModule::class, ViewModelModule::class])
+@Component(modules = [DataModule::class, PresentationModule::class])
 interface ApplicationComponent {
 
     fun inject(mainActivity: MainActivity)
