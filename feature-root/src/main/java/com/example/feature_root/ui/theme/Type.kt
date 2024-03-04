79cb0c6a22b3e188440f6397cddeaa4cb84e0146
RedMuri <celovekda@gmail.com>
2024-03-05 01:38:26 +0300

    Add resources in presentation modules

--- feature-root/src/main/java/com/example/feature_root/ui/theme/Type.kt
+++ feature-root/src/main/java/com/example/feature_root/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.dikidi.ui.theme
+package com.example.feature_root.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
@@ -6,7 +6,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.dikidi.R
+import com.example.feature_root.R
 
 val OpenSans = FontFamily(
     Font(R.font.open_sans_400, FontWeight.Normal),
