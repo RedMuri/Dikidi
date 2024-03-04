6dfbdd7070f8770a0560df10437267591a8ddd1f
RedMuri <celovekda@gmail.com>
2024-03-04 22:54:02 +0300

    Extract Config and navigation property from RootComponent

--- app/src/main/java/com/example/dikidi/ui/root/RootComponent.kt
+++ app/src/main/java/com/example/dikidi/ui/root/RootComponent.kt
@@ -1,11 +1,8 @@
 package com.example.dikidi.ui.root
 
-import android.os.Parcelable
 import com.arkivanov.decompose.router.stack.ChildStack
-import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.value.Value
 import com.example.dikidi.ui.home.HomeComponent
-import kotlinx.parcelize.Parcelize
 
 interface RootComponent {
 
@@ -16,20 +13,9 @@ interface RootComponent {
     sealed interface Child {
 
         data class Home(val component: HomeComponent) : Child
-
         data object Catalog : Child
         data object Sales : Child
         data object Notes : Child
         data object More : Child
-    }
-
-    sealed interface Config : Parcelable {
-
-        @Parcelize data object Home : Config
-
-        @Parcelize data object Catalog : Config
-        @Parcelize data object Sales : Config
-        @Parcelize data object Notes : Config
-        @Parcelize data object More : Config
     }
 }
